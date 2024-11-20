eb5c1bf8c9d1bda59472ef56ee825507de72046e
Mohcine <mohcine-touil@outlook.com>
2024-11-20 12:28:06 +0100

    add file config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$host = 'localhost';
+$db = 'cours_management';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect: " . $e->getMessage());
+}
+?>
