65d01ad65e2f64114a782238335d685b80b36379
Mohcine <mohcine-touil@outlook.com>
2024-11-20 12:23:45 +0100

    add file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestion des Cours</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Bienvenue dans le système de gestion des cours</h1>
+    <a href="login.php">Connexion</a>
+</body>
+</html>
